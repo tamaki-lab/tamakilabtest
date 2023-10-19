b2ad59f3257f87b04e196529c5ffc875b7940916
tsubasa mizuno <t.mizuno.384@nitech.jp>
2023-10-19 13:55:08 +0900

    mizuno_1.pyを変更_1

--- mizuno_1/mizuno_1.py
+++ mizuno_1/mizuno_1.py
@@ -1,7 +1,8 @@
 import datetime
 
-dt_now = datetime.datetime.now()
+def printdt():
+    dt_now = datetime.datetime.now()
 
-dt_str = dt_now.strftime("%Y-%m-%d %H:%M:%S")
+    dt_str = dt_now.strftime("%Y-%m-%d %H:%M:%S")
 
-print("Hello World with Mizuno_" + dt_str)
+    print("Hello World with Mizuno_" + dt_str)
